1783d45ddb9137a04552f5f5754a18d75630deb2
sisdamalia <sisdaamalia@gmail.com>
2018-05-09 21:32:26 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -128,7 +128,7 @@ span.psw {
 <body>
 
 
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto; background-color:aquamarine">Login</button>
 
 <div id="id01" class="modal">
   
@@ -154,7 +154,7 @@ span.psw {
 
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
+      <span class="psw"> <a href="#">Forgot password?</a></span>
     </div>
   </form>
 </div>
