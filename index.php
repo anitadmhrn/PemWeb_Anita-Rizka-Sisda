f8afddb833f4195e9b9f18d2b19397f4aeab4991
sisdamalia <sisdaamalia@gmail.com>
2018-05-14 09:49:48 +0700

    yy

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 <head>
     <title>CRUD</title>
 <head>
-<link rel="stylesheet" href="input.css"/>
+<link rel="stylesheet" href="index.css"/>
 <body>
-    <a href="input.php"> + Tambah Data Baru</a>
-    <h3>Data <i>Reviewer</i></h3>
-    <table class=" table">
+<form style="width: 900px" class="posisi";>
+    <center><h2>Data <i>Reviewer</i></h2></center>
+    <table style="width: 840px;">
         <tr>
             <th>No</th>
             <th>Nama</th>
             <th>Email</th>
             <th>Opsi</th>
         </tr>
+
         <?php
         include "koneksi.php";
         $query_mysql= mysqli_query($host, "SELECT * FROM reviewer")or die(mysql_error());
@@ -21,17 +22,19 @@
         while($data= mysqli_fetch_array($query_mysql)){
             ?>
             <tr>
-                <td><?php echo $nomor++;?></td>
+                <td><center><?php echo $nomor++;?></center></td>
                 <td><?php echo $data['nama_reviewer'];?></td>
                 <td><?php echo $data['email'];?></td>
-                <td>
+                <td><center>
                     <a class="edit" href ="edit.php?id=<?php echo $data ['id'];?>">Edit</a>
                     <a class="hapus" href ="proses_hapus.php?id=<?php echo $data ['id'];?>">Hapus</a>
-                </td>
+            </center></td>
             </tr>
           <?php  
         }
             ?>
-    </table>
+    </table><br>
+    <a href="input.php">Tambah Data Baru</a>
+    </form>
 </body>
 </html>
